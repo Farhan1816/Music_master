909e9ca162c974b6e1c2aa423ed9f214780292de
Farhan <fibnshahid@gmail.com>
2023-11-01 18:51:40 +0600

    gadha

--- app/src/main/java/com/example/myapplication/homeActivity.java
+++ app/src/main/java/com/example/myapplication/homeActivity.java
@@ -1,45 +1,56 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
+import static android.app.PendingIntent.getActivity;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.widget.Toolbar;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
+import android.os.Build;
+import android.graphics.Color;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    //private static final String TAG= HomeActivity.class.getSimpleName();
-    DrawerLayout drawerLayout;
-    BottomNavigationView bottomNavigationView;
+public class homeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
     FragmentManager fragmentManager;
     private Toolbar toolbar;
 
-    @SuppressLint("MissingInflatedId")
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
 
+        View decorView = getWindow().getDecorView();
+        int flags = View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(flags);
+        setContentView(R.layout.activity_home);
 
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //mainly here it handles to go for drawer layout to show
         drawerLayout=findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,
                 R.string.open_nav,R.string.close_nav);
@@ -49,6 +60,20 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView=findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        home hhh = new home();
+        fragmentTransaction.replace(R.id.fragmentContainer, hhh);
+        fragmentTransaction.commit();
+
+
+        final int[] h = {1};
+        final int[] s = { 0 };
+        final int[] l = { 0 };
+
+
         bottomNavigationView=findViewById(R.id.bottom_navigation);
         bottomNavigationView.setBackground(null);
 
@@ -57,21 +82,60 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int itemId = item.getItemId();
                 if (itemId == R.id.bottom_home) {
-                    openFragment(new home());
+                    if(h[0] ==0) {
+                        h[0] = 1;
+                        s[0] = 0;
+                        l[0] = 0;
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.setCustomAnimations(R.anim.enter_animation, R.anim.exit_animation, R.anim.pop_enter_animation, R.anim.pop_exit_animation);
+
+                        home hhh = new home();
+                        fragmentTransaction.replace(R.id.fragmentContainer, hhh);
+                        fragmentTransaction.commit();
+                    }
                     return true;
                 } else if (itemId == R.id.bottom_search) {
-                    openFragment(new Search());
+                        if(s[0] ==0) {
+                            FragmentManager fragmentManager = getSupportFragmentManager();
+                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                            if (h[0] == 1) {
+                                fragmentTransaction.setCustomAnimations(R.anim.enter2_animation, R.anim.exit2_animation, R.anim.pop_enter_animation, R.anim.pop_exit_animation);
+                            } else {
+                                fragmentTransaction.setCustomAnimations(R.anim.enter_animation, R.anim.exit_animation, R.anim.pop_enter_animation, R.anim.pop_exit_animation);
+                            }
+
+                            h[0] = 0;
+                            s[0] = 1;
+                            l[0] = 0;
+
+                            Search search = new Search();
+                            fragmentTransaction.replace(R.id.fragmentContainer, search);
+                            fragmentTransaction.addToBackStack("MyFragmentTag");
+                            fragmentTransaction.commit();
+                        }
                     return true;
                 } else if (itemId == R.id.bottom_library) {
-                    openFragment(new LibraryFragment());
+                    if(l[0] ==0) {
+                        h[0] = 0;
+                        s[0] = 0;
+                        l[0] = 1;
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.setCustomAnimations(R.anim.enter2_animation, R.anim.exit2_animation, R.anim.pop_enter_animation, R.anim.pop_exit_animation);
+
+
+                        LibraryFragment lll = new LibraryFragment();
+                        fragmentTransaction.replace(R.id.fragmentContainer, lll);
+                        fragmentTransaction.commit();
+                    }
                     return true;
                 }
                 return false;
             }
         });
 
-        fragmentManager=getSupportFragmentManager();
-        openFragment(new HomeFragment());
 
     }
 
@@ -105,7 +169,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     private void openFragment(Fragment fragment){
         FragmentTransaction transaction=fragmentManager.beginTransaction();
-        transaction.replace(R.id.fragment_container,fragment);
+        transaction.replace(R.id.fragmentContainer,fragment);
         transaction.commit();
     }
 
